d0c09cae4eeb84df5eace647cdf3b2fc326df9b6
uday634 <uk8096386837@gmail.com>
2023-11-15 13:17:12 +0530

    added styling and variables in the code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,22 @@
-function ExpenseItem(){
-    return (
-        <div>
-        <h2>Expense item!</h2>
-    <ul>
-        <li>Food Rs 10</li>
-        <li>Petrol Rs 100</li>
-        <li>Movies Rs 200</li>
-    </ul>
+import './ExpenseItem.css'
+
+function ExpenseItem() {
+    const expnseDate  = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const localtioOfExpenditure  = 'asr Car Services'
+    const expenseAmount = 259.33;
+
+
+  return (
+    <div className="expnse-item">
+      <h2>Expense item!</h2>
+      <ul>
+        <li className="item">{expnseDate.toISOString} {expenseTitle} {localtioOfExpenditure} {expenseAmount} </li>
+        <li className="item">Petrol Rs 100</li>
+        <li className="item">Movies Rs 200</li> 
+      </ul>
     </div>
-      );
+  );
 }
 
 export default ExpenseItem;
