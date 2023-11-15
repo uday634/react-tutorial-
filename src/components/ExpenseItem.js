da3185b179a2961f9ba2a6360ac1679835ebd480
uday634 <uk8096386837@gmail.com>
2023-11-15 16:53:24 +0530

    used splitting method in react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails'
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
+    
 
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className='expense-item__price'>{props.amount}</div>
-      </div>
+      <ExpenseDate date = {props.date}></ExpenseDate>
+      <ExpenseDetails title = {props.title} location = {props.location} amount = {props.amount}></ExpenseDetails>
     </div>
   );
 }
