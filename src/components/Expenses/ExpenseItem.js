a71684e394746d0a10b6272921319e445322095c
uday634 <uk8096386837@gmail.com>
2023-11-20 20:49:56 +0530

    added event listenr

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,10 +4,16 @@ import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  const clickHandler = () => {
+    console.log('Clicked!!!!');
+  };
+
+
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}></ExpenseDate>
       <ExpenseDetails title = {props.title} location = {props.location} amount = {props.amount}></ExpenseDetails>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
