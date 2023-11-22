4850d3ac0fa3c6c84dfbae3de83f58177ca447af
uday634 <uk8096386837@gmail.com>
2023-11-22 11:04:50 +0530

    added useState hook in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,24 @@
+import React, {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
+  // function clickHandeler() {}
+
+  const [amount, setTitle] = useState(props.amount); //userState is the React hook ( it return an array)
+
   const clickHandler = () => {
-    console.log('Clicked!!!!');
+    setTitle('100'); //useState does not update value right away but it will scedule it. 
+    console.log(amount);
   };
 
 
   return (
     <Card className="expense-item">
       <ExpenseDate date = {props.date}></ExpenseDate>
-      <ExpenseDetails title = {props.title} location = {props.location} amount = {props.amount}></ExpenseDetails>
+      <ExpenseDetails title = {props.title} location = {props.location} amount = {amount}></ExpenseDetails>
       <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
