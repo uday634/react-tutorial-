d74b3b43ffd9ce3a526f1befdf6f8c88e12c0626
uday634 <uk8096386837@gmail.com>
2023-11-23 18:39:52 +0530

    created the object which have the input of the form field

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,22 +3,53 @@ import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
 
-  const [enteredTitle, setEnteredTitle] = useState('')
-  const [enteredAmount, setEnteredAmount] = useState('')
-  const [enteredDate, setEnteredDate] = useState('')
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle:'',
+  //   enteredAmount:'',
+  //   enteredDate: ''
+  // })
+
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+
+    // setUserInput((prevState)=>{
+    //   return { ...prevState, enteredTitle: event.target.value};
+    // });
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput, enteredAmount: event.target.value,
+    // });
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput, enteredDate: event.target.value,
+    // });
   };
+  const SubmitHandler = (event) =>{
+    event.preventDefault();
+    let expenseData = {
+      title: enteredTitle,
+      amount:enteredAmount,
+      date:new Date(enteredDate)
+    };
+    console.log(expenseData)
+  }
+
 
 
   return (
-    <form>
+    <form onSubmit={SubmitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Expense title</label>
@@ -40,7 +71,7 @@ const ExpenseForm = () => {
           />
         </div>
         <div className="new-expense__actions">
-          <button type="submit" className="btn" onClick={titleChangeHandler}>Add</button>
+          <button type="submit" className="btn" onClick={SubmitHandler}>Add</button>
         </div>
       </div>
     </form>
