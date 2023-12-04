0b0153e871a3431e7caf753b411d0eb73f8eab71
uday634 <uk8096386837@gmail.com>
2023-12-04 18:41:38 +0530

    added char

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -3,7 +3,13 @@ import React from "react";
 import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
 import Card from "../UI/Card";
 import ExepnsesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+
 import "./Expense.css";
+
+
+
 let filteredExpenses 
 
 const Expenses = (props) => {
@@ -26,6 +32,7 @@ const Expenses = (props) => {
     <div>
       <Card>
         <ExpensesFilter yearFilter={yearFilterHandler} />
+        <ExpensesChart expenses= {filteredExpenses}></ExpensesChart>
         <ExepnsesList filteredExpenses={filteredExpenses}/>
       </Card>
     </div>
